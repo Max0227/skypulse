733e40a772fe960438147ba20fe7b0f3eb6fc646
Max0227 <3dladesign@gmail.com>
2026-03-14 18:17:25 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import './styles.css';
 import './game.js';
 
-// Инициализация Telegram Mini App
 const tg = window.Telegram?.WebApp;
 if (tg) {
-  tg.ready();                // Сообщаем Telegram, что приложение готово
-  tg.expand();               // Растягиваем на весь экран
-  // Можно установить цвета (опционально)
-  tg.setHeaderColor?.('#030712');
-  tg.setBackgroundColor?.('#030712');
+  tg.ready();
+  tg.expand();
 }
